d71b88db09a56fb76386dc07c76513499e1e0fae
EdwART <122360856+EdwARTSs@users.noreply.github.com>
2023-01-12 22:37:21 +0800

    Add files via upload

---
+++ QuizGameLESSON2.js
@@ -0,0 +1,94 @@
+/*QUESTIONS FOR THE QUIZ LESSON 2*/
+
+const questions = [
+    {
+        question: 'Stands for Cascading Stylesheet',
+        answers: [
+            {text: 'CSS', correct: true},
+            {text: 'CS', correct: false},
+            {text: 'CCS', correct: false},
+            {text: 'CCSS', correct: false},
+        ]
+    },
+    {
+		question: 'This describes how HTML elements are to be displayed on screen',
+		answers: [
+			{text: 'HTML', correct: false},
+			{text: 'CSS', correct: true},
+			{text: 'PHP', correct: false},
+			{text: 'JS', correct: false}
+		]
+	},
+    {
+		question: 'This CSS property specifies the background color of an element',
+		answers: [
+			{text: 'background_color', correct: false},
+			{text: 'color.background', correct: false},
+			{text: 'background-color', correct: true},
+			{text: 'coloring-background', correct: false}
+		]
+	},
+    {
+		question: 'This CSS property specifies the color of the text',
+		answers: [
+			{text: 'color()', correct: false},
+			{text: 'html.color', correct: false},
+			{text: 'coloring', correct: false},
+			{text: 'color', correct: true}
+		]
+	},
+    {
+		question: 'This CSS property specifies the horizontal alignment of text in an element',
+		answers: [
+			{text: 'text-align', correct: true},
+			{text: 'align.text', correct: false},
+			{text: 'txt-align', correct: false},
+			{text: 'texts-align', correct: false}
+		]
+	},
+    {
+		question: 'This CSS property specifies the font for an element',
+		answers: [
+			{text: 'fonts-family', correct: false},
+			{text: 'font-family', correct: true},
+			{text: 'font-style', correct: false},
+			{text: 'fonts-style', correct: false}
+		]
+	},
+    {
+		question: 'This CSS property specifies the size of a font',
+		answers: [
+			{text: 'size-font', correct: false},
+			{text: 'fonts.size', correct: false},
+			{text: 'font-size', correct: true},
+			{text: 'fonts-size', correct: false}
+		]
+	},
+    {
+		question: 'This HTML tag is used to define style information(CSS) for a document',
+		answers: [
+			{text: '<styles>', correct: false},
+			{text: '<stylish>', correct: false},
+			{text: '<styling>', correct: false},
+			{text: '<style>', correct: true}
+		]
+	},
+    {
+		question: 'External stylesheets are stored in?',
+		answers: [
+			{text: 'CSS files', correct: true},
+			{text: 'HTML files', correct: false},
+			{text: 'JS files', correct: false},
+			{text: 'PHP files', correct: false}
+		]
+	},
+    {
+		question: 'This is the file format when saving a stylesheet',
+		answers: [
+			{text: 'filename.cs', correct: false},
+			{text: 'filename.css', correct: true},
+			{text: 'filename.ccs', correct: false},
+			{text: 'filename.stylesheet', correct: false}
+		]
+	}
+]
